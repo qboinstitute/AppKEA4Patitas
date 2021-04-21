57d643d61419af70c412e822eb41515a3444a5c4
LuisAngel <luisangel.salvat@gmail.com>
2021-04-20 22:13:16 -0500

    4to. avance LoginActivity terminado.

--- app/src/main/java/com/qbo/appkea4patitas/view/LoginActivity.kt
+++ app/src/main/java/com/qbo/appkea4patitas/view/LoginActivity.kt
@@ -1,12 +1,20 @@
 package com.qbo.appkea4patitas.view
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import com.google.android.material.snackbar.Snackbar
 import com.qbo.appkea4patitas.R
 import com.qbo.appkea4patitas.databinding.ActivityLoginBinding
 import com.qbo.appkea4patitas.databinding.ActivityMainBinding
+import com.qbo.appkea4patitas.retrofit.PatitasCliente
+import com.qbo.appkea4patitas.retrofit.request.RequestLogin
+import com.qbo.appkea4patitas.retrofit.response.ResponseLogin
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class LoginActivity : AppCompatActivity() {
 
@@ -16,7 +24,45 @@ class LoginActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        binding.btnlogin.setOnClickListener {
+            binding.btnlogin.isEnabled = false
+            if(validarUsuarioPassword()){
+                autenticarUsuario(it, binding.etusuariologin.text.toString(),
+                binding.etpasswordlogin.text.toString())
+            }else{
+                binding.btnlogin.isEnabled = true
+                mensaje(it, getString(R.string.msgusupassword))
+            }
+        }
+        binding.tvregistrarusuario.setOnClickListener {
+            startActivity(Intent(applicationContext,
+            RegistroActivity::class.java))
+        }
     }
+
+    fun autenticarUsuario(vista: View, usuario: String, password: String){
+        val requestLogin = RequestLogin(usuario, password)
+        val call : Call<ResponseLogin> = PatitasCliente
+            .retrofitServicio
+            .login(requestLogin)
+        call.enqueue(object : Callback<ResponseLogin> {
+            override fun onResponse(call: Call<ResponseLogin>, response: Response<ResponseLogin>) {
+                if(response.body()!!.rpta){
+                    //mensaje(vista, "Login success!!")
+                    startActivity(Intent(applicationContext,
+                        HomeActivity::class.java))
+                    finish()
+                }
+            }
+            override fun onFailure(call: Call<ResponseLogin>, t: Throwable) {
+                Log.e("ErrorWS", t.message)
+                mensaje(vista, "Login failed!!")
+            }
+
+        })
+        binding.btnlogin.isEnabled = true
+    }
+
 
     fun validarUsuarioPassword() : Boolean {
         var respuesta = true
