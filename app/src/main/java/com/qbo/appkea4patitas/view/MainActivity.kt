ad28375e8648886183a10a86d3bf511f6168f1c4
LuisAngel <luisangel.salvat@gmail.com>
2021-04-20 19:14:49 -0500

    1er. avance splash activity.

--- app/src/main/java/com/qbo/appkea4patitas/view/MainActivity.kt
+++ app/src/main/java/com/qbo/appkea4patitas/view/MainActivity.kt
@@ -1,12 +1,19 @@
 package com.qbo.appkea4patitas.view
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 import com.qbo.appkea4patitas.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Handler().postDelayed({
+            startActivity(
+                    Intent(this, LoginActivity::class.java))
+            finish()
+        }, 3000)
     }
 }
